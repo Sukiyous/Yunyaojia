8e9f7e9f7bb39838486cf4831df5802a3b69631d
Sukiyous <193663514@qq.com>
2025-05-31 05:10:38 +0800

    one

---
+++ packages/vuepress-theme-reco/src/client/components/Home/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer class="footer">
+    djc-2025
+  </footer>
+</template>
+
+<script setup>
+// 这里可以添加脚本逻辑（如有需要）
+</script>
+
+<style scoped>
+.footer {
+  text-align: center;
+  color: #888;
+  padding: 16px 0;
+  font-size: 14px;
+}
+</style> 
