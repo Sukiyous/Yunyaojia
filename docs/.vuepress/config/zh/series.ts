3136a10daea5fe9ddfea7b11e04d6b5e9f7a9172
Sukiyous <193663514@qq.com>
2025-05-31 04:17:56 +0800

    caidan

--- docs/.vuepress/config/zh/series.ts
+++ docs/.vuepress/config/zh/series.ts
@@ -1,5 +1,6 @@
 export const series = {
   // todo README.md 无法展示，没有对 '' 进行处理
+
   //指南页面侧边栏设置
   '/docs/guide/': [
     {
@@ -8,14 +9,50 @@ export const series = {
     },
     {
       text:'基础档案',
-      children:['getting-started'],
+      children:['/docs/guide/jichudangan/shangpinguanli'],
     },
-    
     {
-      text:'测试',
-      children:['gui-builder','package-manager','custom-style','style','icon','bundler','builtin-page','folder-specification','register-components','register-layouts','contribute'],
-    }
+      text:'库存管理',
+      children:[
+        '/docs/guide/kucunguanli/ruku',
+        '/docs/guide/kucunguanli/sunyi',
+      ],
+    },
+    {
+      text:'零售促销',
+      children:['/docs/guide/lingshoucuxiao/lingshou'],
+    },
+    {
+      text:'GSP质管',
+      children:['/docs/guide/GSPzhiguan/yanghujihua'],
+    },
+    {
+      text:'医保管理',
+      children:['/docs/guide/yibaoguanli/yibaojiesuanjilu'],
+    },
+    {
+      text:'溯源管理',
+      children:['/docs/guide/suyuanguanli/suyuanguanli'],
+    },
+    {
+      text:'综合查询',
+      children:['/docs/guide/zonghechaxun/rukuchaxun'],
+    },
+    {
+      text:'报表分析',
+      children:['/docs/guide/baobiaofenxi/baobiaofenxi'],
+    },
+    {
+      text:'设置中心',
+      children:['/docs/guide/shezhizhongxin/yonghuguanli'],
+    },
+    {
+      text:'管理员',
+      children:['/docs/guide/guanliyuan/admin'],
+    },
   ],
+
+
 
   //参考页面侧边栏设置
   '/docs/theme/': [
